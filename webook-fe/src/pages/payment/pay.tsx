0792d631842b146ca82fc77c7439933e95107e5e
up-to-star <2200316527@qq.com>
2024-11-08 21:55:33 +0800

    docker-compose 初始化数据库

--- webook-fe/src/pages/payment/pay.tsx
+++ webook-fe/src/pages/payment/pay.tsx
@@ -1,3 +1,3 @@
 function Page() {
-    return ()
+    return ("")
 }
--- webook/main.go
+++ webook/main.go
@@ -2,11 +2,29 @@ package main
 
 import (
 	"basic-project/webook/internal/web"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"strings"
+	"time"
 )
 
 func main() {
 	server := gin.Default()
+	server.Use(cors.New(cors.Config{
+		//AllowOrigins: []string{"http://localhost:3000"},
+		//AllowMethods: []string{"PUT", "PATCH", "POST"},
+		AllowHeaders: []string{"Content-Type", "Authorization"},
+		//ExposeHeaders:    []string{"Content-Type", "Authorization"},
+		// 是否允许带 cookie 之类的东西
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			if strings.Contains(origin, "http://localhost") {
+				return true
+			}
+			return strings.Contains(origin, "www.xxx.com")
+		},
+		MaxAge: 12 * time.Hour,
+	}))
 	u := web.NewUserHandle()
 	u.RegisterRoutes(server)
 	_ = server.Run(":8080")
