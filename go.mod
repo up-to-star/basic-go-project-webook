ff76419b6d5b580d7d49e41a554c2eaba24ccf11
up-to-star <2200316527@qq.com>
2024-11-19 21:02:12 +0800

    单元测试 dao Insert (要用到sqlmock)

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module basic-project
 go 1.22
 
 require (
+	github.com/DATA-DOG/go-sqlmock v1.5.2
 	github.com/dlclark/regexp2 v1.11.4
 	github.com/gin-contrib/cors v1.7.2
 	github.com/gin-contrib/sessions v1.0.1
---
+++ webook/internal/repository/dao/user_test.go
@@ -0,0 +1,53 @@
+package dao
+
+import (
+	"context"
+	"database/sql"
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"testing"
+)
+
+func TestGORMUserDAO_Insert(t *testing.T) {
+	testCases := []struct {
+		name    string
+		user    User
+		mock    func(t *testing.T) *sql.DB
+		wantErr error
+	}{
+		{
+			name: "插入成功",
+			mock: func(t *testing.T) *sql.DB {
+				mockDB, mock, err := sqlmock.New()
+				require.NoError(t, err)
+				res := sqlmock.NewResult(3, 1)
+				mock.ExpectExec("INSERT INTO `users` .*").WillReturnResult(res)
+				return mockDB
+			},
+			user: User{
+				Email: sql.NullString{
+					String: "test@example.com",
+					Valid:  true,
+				},
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			db, err := gorm.Open(mysql.New(mysql.Config{
+				Conn:                      tc.mock(t),
+				SkipInitializeWithVersion: true,
+			}), &gorm.Config{
+				DisableAutomaticPing:   true,
+				SkipDefaultTransaction: true,
+			})
+			d := NewUserDAO(db)
+			err = d.Insert(context.Background(), tc.user)
+			assert.Equal(t, tc.wantErr, err)
+		})
+	}
+}
