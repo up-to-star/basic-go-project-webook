759125d760e738cab64aeeeaa3023ebc24005413
up-to-star <22003165277@11.com>
2024-11-20 10:31:17 +0800

    单元测试 dao/Insert

--- webook/internal/repository/dao/user_test.go
+++ webook/internal/repository/dao/user_test.go
@@ -4,9 +4,10 @@ import (
 	"context"
 	"database/sql"
 	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/go-sql-driver/mysql"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"gorm.io/driver/mysql"
+	gormMysql "gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"testing"
 )
@@ -34,11 +35,24 @@ func TestGORMUserDAO_Insert(t *testing.T) {
 				},
 			},
 		},
+		{
+			name: "邮箱冲突",
+			mock: func(t *testing.T) *sql.DB {
+				mockDB, mock, err := sqlmock.New()
+				require.NoError(t, err)
+				mock.ExpectExec("INSERT INTO `users` .*").WillReturnError(&mysql.MySQLError{
+					Number: 1062,
+				})
+				return mockDB
+			},
+			user:    User{},
+			wantErr: ErrUserDuplicate,
+		},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			db, err := gorm.Open(mysql.New(mysql.Config{
+			db, err := gorm.Open(gormMysql.New(gormMysql.Config{
 				Conn:                      tc.mock(t),
 				SkipInitializeWithVersion: true,
 			}), &gorm.Config{
