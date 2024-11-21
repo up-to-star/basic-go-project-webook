2a1eb4447c04574c0a434fa0617847bac5d2a65d
up-to-star <2200316527@qq.com>
2024-11-21 16:41:06 +0800

    第三方服务治理 切换不同服务

---
+++ webook/internal/service/failover/service.go
@@ -0,0 +1,44 @@
+package failover
+
+import (
+	"basic-project/webook/internal/service/sms"
+	"context"
+	"errors"
+	"log"
+	"sync/atomic"
+)
+
+type FailoverSMSService struct {
+	svcs []sms.Service
+	idx  uint64
+}
+
+func NewFailoverSMSService(svcs []sms.Service) sms.Service {
+	return &FailoverSMSService{
+		svcs: svcs,
+	}
+}
+
+func (f *FailoverSMSService) Send(ctx context.Context, tplId string, args []string, numbers ...string) error {
+	//for _, svc := range f.svcs {
+	//	err := svc.Send(ctx, tplId, args, numbers...)
+	//	if err == nil {
+	//		return nil
+	//	}
+	//	log.Panicln(err)
+	//}
+	// 轮训
+	idx := atomic.AddUint64(&f.idx, 1)
+	length := uint64(len(f.svcs))
+	for i := idx; i < idx+length; i++ {
+		err := f.svcs[int(i%length)].Send(ctx, tplId, args, numbers...)
+		switch {
+		case err == nil:
+			return nil
+		case errors.Is(err, context.DeadlineExceeded), errors.Is(err, context.Canceled):
+			return err
+		}
+		log.Println(err)
+	}
+	return errors.New("发送失败，全部服务都失败了")
+}
---
+++ webook/internal/service/failover/time_out_failover.go
@@ -0,0 +1,38 @@
+package failover
+
+import (
+	"basic-project/webook/internal/service/sms"
+	"context"
+	"errors"
+	"log"
+	"sync/atomic"
+)
+
+type TimeOutFailoverSMSService struct {
+	svcs      []sms.Service
+	cnt       uint32
+	idx       uint32
+	threshold uint32
+}
+
+func (t *TimeOutFailoverSMSService) Send(ctx context.Context, tplId string, args []string, numbers ...string) error {
+	cnt := atomic.LoadUint32(&t.cnt)
+	idx := atomic.LoadUint32(&t.idx)
+	if cnt > t.threshold {
+		newIdx := (idx + 1) % uint32(len(t.svcs))
+		if atomic.CompareAndSwapUint32(&t.idx, idx, newIdx) {
+			atomic.StoreUint32(&t.cnt, 0)
+		}
+	}
+	svc := t.svcs[int(idx)]
+	err := svc.Send(ctx, tplId, args, numbers...)
+	switch {
+	case err == nil:
+		atomic.StoreUint32(&t.cnt, 0)
+	case errors.Is(err, context.DeadlineExceeded):
+		atomic.AddUint32(&t.cnt, 1)
+	default:
+		log.Println("保持不动")
+	}
+	return nil
+}
