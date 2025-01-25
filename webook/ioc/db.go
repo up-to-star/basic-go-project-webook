278df37a16443ce1095a86934ae6f0078301c65c
up-to-star <2200316527@qq.com>
2025-01-25 15:40:47 +0800

    gorm监控执行时间上报Prometheus

--- webook/ioc/db.go
+++ webook/ioc/db.go
@@ -2,12 +2,14 @@ package ioc
 
 import (
 	"basic-project/webook/internal/repository/dao"
+	prometheus2 "github.com/prometheus/client_golang/prometheus"
 	"github.com/spf13/viper"
 	"go.uber.org/zap"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"gorm.io/plugin/prometheus"
 	"moul.io/zapgorm2"
+	"time"
 )
 
 func InitDB() *gorm.DB {
@@ -40,6 +42,10 @@ func InitDB() *gorm.DB {
 	if err != nil {
 		panic(err)
 	}
+
+	// 监控查询的执行时间
+	pcb := newCallbacks()
+	pcb.registerAll(db)
 	err = dao.InitTable(db)
 	if err != nil {
 		panic(err)
@@ -61,4 +67,96 @@ func InitDBDefault() *gorm.DB {
 		panic(err)
 	}
 	return db
+}
+
+type Callbacks struct {
+	vector *prometheus2.SummaryVec
+}
+
+func newCallbacks() *Callbacks {
+	vector := prometheus2.NewSummaryVec(prometheus2.SummaryOpts{
+		Namespace: "study_webook",
+		Subsystem: "webook",
+		Name:      "gorm_query_time",
+		Help:      "统计 GORM 执行时间",
+		ConstLabels: map[string]string{
+			"db": "webook",
+		},
+		Objectives: map[float64]float64{
+			0.5:   0.01,
+			0.75:  0.01,
+			0.9:   0.01,
+			0.99:  0.001,
+			0.999: 0.0001,
+		},
+	}, []string{"type", "table"})
+	prometheus2.MustRegister(vector)
+	return &Callbacks{
+		vector: vector,
+	}
+}
+
+func (cb *Callbacks) before() func(db *gorm.DB) {
+	return func(db *gorm.DB) {
+		startTime := time.Now()
+		db.Set("start_time", startTime)
+	}
+}
+
+func (cb *Callbacks) after(typ string) func(db *gorm.DB) {
+	return func(db *gorm.DB) {
+		val, _ := db.Get("start_time")
+		startTime, ok := val.(time.Time)
+		if !ok {
+			return
+		}
+		table := db.Statement.Table
+		if table == "" {
+			table = "unknown"
+		}
+		cb.vector.WithLabelValues(typ, table).Observe(float64(time.Since(startTime).Milliseconds()))
+	}
+}
+
+func (cb *Callbacks) registerAll(db *gorm.DB) {
+	err := db.Callback().Create().Before("*").Register("prometheus_before_create", cb.before())
+	if err != nil {
+		panic(err)
+	}
+	err = db.Callback().Create().After("*").Register("prometheus_after_create", cb.after("create"))
+	if err != nil {
+		panic(err)
+	}
+	err = db.Callback().Query().Before("*").Register("prometheus_before_query", cb.before())
+	if err != nil {
+		panic(err)
+	}
+	err = db.Callback().Query().After("*").Register("prometheus_after_query", cb.after("query"))
+	if err != nil {
+		panic(err)
+	}
+	err = db.Callback().Delete().Before("*").Register("prometheus_before_delete", cb.before())
+	if err != nil {
+		panic(err)
+	}
+	err = db.Callback().Delete().After("*").Register("prometheus_after_delete", cb.after("delete"))
+	if err != nil {
+		panic(err)
+	}
+	err = db.Callback().Update().Before("*").Register("prometheus_before_update", cb.before())
+	if err != nil {
+		panic(err)
+	}
+	err = db.Callback().Update().After("*").Register("prometheus_after_update", cb.after("update"))
+	if err != nil {
+		panic(err)
+	}
+	err = db.Callback().Row().Before("*").Register("prometheus_before_row", cb.before())
+	if err != nil {
+		panic(err)
+	}
+	err = db.Callback().Row().After("*").Register("prometheus_after_row", cb.after("row"))
+	if err != nil {
+		panic(err)
+	}
 }
